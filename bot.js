36e5aa2611200fb4713ce355cbcf4b9b89f18d12
Boris Annink <88558971+Borito185@users.noreply.github.com>
2026-02-17 23:11:32 +0100

    Prepared lib for adding script running multiple farms

--- bot.js
+++ bot.js
@@ -19,21 +19,68 @@ const { control } = require("./lib/control")
 const { action } = require("./lib/action")
 const { progress } = require("./lib/progress")
 
-module.exports = {
-    start(farm_name=null) {
-        logger.info("Started farming!");
-        bot_state.farm_name = farm_name
-        if (bot_state.farm_name !== null) {
-            Chat.say("/g IF-AgribotUpdates " + bot_state.farm_name + " | started")
-        }
-    },
-    finish() {
+const farms = []
+let farm_depth = 0
+
+function start(farm_name=null) {
+    farm_depth++;
+    if (farm_depth === 1) logger.info("Started farming!");
+    bot_state.farm_name = farm_name
+    if (bot_state.farm_name !== null) {
+        Chat.say("/g IF-AgribotUpdates " + bot_state.farm_name + " | started")
+    }
+}
+
+function finish() {
+    farm_depth--;
+    if (farm_depth === 0) {
         logger.alert("Finished farming!");
         Chat.say("/logout");
+    }
 
-        if (bot_state.farm_name !== null) {
-            Chat.say("/g IF-AgribotUpdates " + bot_state.farm_name + " | finished")
-            bot_state.farm_name = null
+    if (bot_state.farm_name !== null) {
+        Chat.say("/g IF-AgribotUpdates " + bot_state.farm_name + " | finished")
+        bot_state.farm_name = null
+    }
+}
+
+module.exports = {
+    add_farm(x, y, z, name, script) {
+        farms.push({
+            pos: PositionCommon.createPos(x, y, z),
+            name,
+            script
+        })
+    },
+
+     run_farm() {
+         const pos = Player.getPlayer().getPos()
+         pos.x = Math.floor(pos.x); pos.y = Math.floor(pos.y); pos.z = Math.floor(pos.z);
+
+         const farm = farms.find(f =>
+             f.pos.x === pos.x &&
+             f.pos.y === pos.y &&
+             f.pos.z === pos.z
+         )
+
+         if (!farm) {
+             logger.debug("Farm not found. Make sure you are standing on the right block.")
+             return
+         }
+
+         start(farm.name)
+
+         try {
+             farm.script()   // this works if the file exports a function
+         } finally {
+             finish()
+         }
+    },
+
+    toggle_paused(new_value = !(GlobalVars.getBoolean("bot_is_paused") ?? false)) {
+        GlobalVars.putBoolean("bot_is_paused", new_value)
+        if (new_value) {
+            logger.info("Restarting...")
         }
     },
 
--- run.js
+++ run.js
@@ -1,57 +1,17 @@
 const bot = require("./bot")
-const farms = []
 
-add_farm(-4783, 161, 7789, null, require("./glenda/spruce"))
-add_farm(-4921, 123, 7824, null, require("./glenda/sweetberry"))
-add_farm(-8619, 68, 1349, null, require("./snapper/jungle"))
-add_farm(-9527, 68, 2419, "Karydia Wheat", require("./karydia/wheat"))
-add_farm(-8931, 107, 2381, "Karydia Jungle", require("./karydia/jungle"))
-add_farm(-3281, 120, 8037, null, require("./nether/warped"))
-add_farm(-3281, 85, 8037, null, require("./nether/warped"))
-add_farm(-3281, 50, 8037, null, require("./nether/warped"))
-add_farm(-2995, 79, 9116, null, require("./florabis/potato"))
-add_farm(-2908, 74, 8042, null, require("./aethia/vines"))
+bot.add_farm(-4783, 161, 7789, null, require("./glenda/spruce"))
+bot.add_farm(-4921, 123, 7824, null, require("./glenda/sweetberry"))
+bot.add_farm(-8619, 68, 1349, null, require("./snapper/jungle"))
+bot.add_farm(-9527, 68, 2419, "Karydia Wheat", require("./karydia/wheat"))
+bot.add_farm(-8931, 107, 2381, "Karydia Jungle", require("./karydia/jungle"))
+bot.add_farm(-3281, 120, 8037, null, require("./nether/warped"))
+bot.add_farm(-3281, 85, 8037, null, require("./nether/warped"))
+bot.add_farm(-3281, 50, 8037, null, require("./nether/warped"))
+bot.add_farm(-2995, 79, 9116, null, require("./florabis/potato"))
+bot.add_farm(-2908, 74, 8042, null, require("./aethia/vines"))
 
-function main() {
-    if (isAlreadyRunning()) {
-        const isPaused = GlobalVars.getBoolean("bot_is_paused") ?? false
-        GlobalVars.putBoolean("bot_is_paused", !isPaused)
-        if (isPaused) {
-            bot.logger.info("Restarting...")
-        }
-
-        return
-    }
-
-    GlobalVars.putBoolean("bot_is_running", true)
-
-    const player = Player.getPlayer()
-    const pos = player.getPos()
-    pos.x = Math.floor(pos.x)
-    pos.y = Math.floor(pos.y)
-    pos.z = Math.floor(pos.z)
-
-    const farm = farms.find(f =>
-        f.pos.x === pos.x &&
-        f.pos.y === pos.y &&
-        f.pos.z === pos.z
-    )
-
-    if (!farm) {
-        bot.logger.debug("Farm not found. Make sure you are standing on the right block.")
-        return
-    }
-
-    bot.start(farm.name)
-
-    try {
-        farm.script()   // this works if the file exports a function
-    } finally {
-        bot.finish()
-    }
-}
-
-function isAlreadyRunning() {
+function is_already_running() {
     const current = context.file
 
     return JsMacros.getOpenContexts()
@@ -59,12 +19,10 @@ function isAlreadyRunning() {
         .length > 1
 }
 
-function add_farm(x, y, z, name, script) {
-    farms.push({
-        pos: PositionCommon.createPos(x, y, z),
-        name,
-        script
-    })
+if (is_already_running()) {
+    bot.toggle_paused()
+
+    return
 }
 
-main()
+bot.run_farm()
